da9d6538387c4680c96a310b60959bc46151aea2
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-04-21 16:36:04 -0300

    feat: creating footer content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { ChevronLeft, ChevronRight, Home as HomeIcon, Library, Play, Search } from "lucide-react";
+import { ChevronLeft, ChevronRight, Home as HomeIcon, Laptop2, LayoutList, Library, Maximize2, Mic2, Play, Repeat, Search, Shuffle, SkipBack, SkipForward, Volume } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -126,7 +126,7 @@ export default function Home() {
               </span>
             </a>
             <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
-            <Image src="/album.jpg" className="w-full" width={104} height={104} alt="Capa do álbum Wasting Light da banda Foo Fighters" />
+              <Image src="/album.jpg" className="w-full" width={104} height={104} alt="Capa do álbum Wasting Light da banda Foo Fighters" />
               <strong className="font-semibold">
                 Daily Mix 1
               </strong>
@@ -146,7 +146,52 @@ export default function Home() {
           </div>
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">footer</footer>
+      <footer className="bg-zinc-900 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <Image src="/album.jpg" className="w-full" width={56} height={56} alt="Capa do álbum Wasting Light da banda Foo Fighters" />
+          <div className="flex flex-col">
+            <strong className="font-normal">
+              Rope
+            </strong>
+            <span className="text-xs text-zinc-400">
+              Foo Fighters
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <div className="flex items-center gap-3">
+            <Shuffle size={20} className="text-zinc-200" />
+            <SkipBack size={20} className="text-zinc-200" />
+            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black ml-auto">
+              <Play />
+            </button>
+            <SkipForward size={20} className="text-zinc-200" />
+            <Repeat size={20} className="text-zinc-200" />
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc-400">0:31</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600">
+              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>
+            </div>
+            <span className="text-xs text-zinc-400">2:14</span>
+          </div>
+        </div>
+
+        <div>
+          <div className="flex items-center gap-4">
+            <Mic2 size={20} />
+            <LayoutList size={20} />
+            <Laptop2 size={20} />
+            <div className="flex items-center gap-2">
+              <Volume size={20} />
+              <div className="h-1 rounded-full w-24 bg-zinc-600">
+                <div className="bg-zinc-200 w-10 h-1 rounded-full"></div>
+              </div>
+            </div>
+            <Maximize2 size={20} />
+          </div>
+        </div>
+      </footer>
     </div>
   )
 }
