d6b5693debc5bfeac51c2f3c43a6db7af8e3f8b7
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-04-21 16:13:14 -0300

    feat: creating main content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Home as HomeIcon, Library, Search } from "lucide-react";
+import { ChevronLeft, ChevronRight, Home as HomeIcon, Library, Play, Search } from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -33,7 +34,117 @@ export default function Home() {
             <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Rap Americano</a>
           </nav>
         </aside>
-        <main className="flex-1 p-6">main</main>
+        <main className="flex-1 p-6">
+          <div className="flex items-center gap-3">
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronLeft />
+            </button>
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronRight />
+            </button>
+
+          </div>
+          <h1 className="font-semibold text-3xl mt-10">
+            Good Afternoon
+          </h1>
+
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <a href="#" className="bg-white/10 group flex items-center gap-4 rounded-md hover:bg-white/20 transition-colors" overflow-hidden>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum Wasting Light da banda Foo Fighters" />
+              <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a href="#" className="bg-white/10 group flex items-center gap-4 rounded-md hover:bg-white/20 transition-colors" overflow-hidden>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum Wasting Light da banda Foo Fighters" />
+              <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a href="#" className="bg-white/10 group flex items-center gap-4 rounded-md hover:bg-white/20 transition-colors" overflow-hidden>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum Wasting Light da banda Foo Fighters" />
+              <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a href="#" className="bg-white/10 group flex items-center gap-4 rounded-md hover:bg-white/20 transition-colors" overflow-hidden>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum Wasting Light da banda Foo Fighters" />
+              <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a href="#" className="bg-white/10 group flex items-center gap-4 rounded-md hover:bg-white/20 transition-colors" overflow-hidden>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum Wasting Light da banda Foo Fighters" />
+              <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a href="#" className="bg-white/10 group flex items-center gap-4 rounded-md hover:bg-white/20 transition-colors" overflow-hidden>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum Wasting Light da banda Foo Fighters" />
+              <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+          </div>
+
+          <h2 className="font-semibold text-2xl mt-10">
+            Made for Felipe Bastos
+          </h2>
+
+          <div className="grid grid-cols-8 gap-4 mt-4">
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+              <Image src="/album.jpg" className="w-full" width={104} height={104} alt="Capa do álbum Wasting Light da banda Foo Fighters" />
+              <strong className="font-semibold">
+                Daily Mix 1
+              </strong>
+              <span className="text-sm text-zinc-400">
+                Wallos, COIN, girl in red and more
+              </span>
+            </a>
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+              <Image src="/album.jpg" className="w-full" width={104} height={104} alt="Capa do álbum Wasting Light da banda Foo Fighters" />
+              <strong className="font-semibold">
+                Daily Mix 1
+              </strong>
+              <span className="text-sm text-zinc-400">
+                Wallos, COIN, girl in red and more
+              </span>
+            </a>
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+              <Image src="/album.jpg" className="w-full" width={104} height={104} alt="Capa do álbum Wasting Light da banda Foo Fighters" />
+              <strong className="font-semibold">
+                Daily Mix 1
+              </strong>
+              <span className="text-sm text-zinc-400">
+                Wallos, COIN, girl in red and more
+              </span>
+            </a>
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+            <Image src="/album.jpg" className="w-full" width={104} height={104} alt="Capa do álbum Wasting Light da banda Foo Fighters" />
+              <strong className="font-semibold">
+                Daily Mix 1
+              </strong>
+              <span className="text-sm text-zinc-400">
+                Wallos, COIN, girl in red and more
+              </span>
+            </a>
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+              <Image src="/album.jpg" className="w-full" width={104} height={104} alt="Capa do álbum Wasting Light da banda Foo Fighters" />
+              <strong className="font-semibold">
+                Daily Mix 1
+              </strong>
+              <span className="text-sm text-zinc-400">
+                Wallos, COIN, girl in red and more
+              </span>
+            </a>
+          </div>
+        </main>
       </div>
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6">footer</footer>
     </div>
